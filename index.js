548f5c93edbd52a4612d75e314e768a65f5849dc
lee <leesumiir@gmail.com>
2018-04-03 17:12:32 +0800

    feat: 新增 ajv 支持，替换原先的数据校验

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const inflection = require('path-proxy/node_modules/inflection')
 const pathProxy = require('path-proxy')
 const promisifyCall = require('promisify-call')
+const ajv = require('./ajv')
+const _ = require('lodash')
 
 class Builder {
   constructor (baseObj, resources) {
@@ -21,7 +22,6 @@ class Builder {
   buildAction (action) {
     const actionName = action.title
     const properties = action.properties
-    const requiredProps = action.required
 
     // HACKY special case for members bulk add and send MIME endpoints
     const path = action.href.replace(/\.json/gi, '').replace(/\.mime/gi, '').replace(/:.*/g, '').replace(/\/path$/g, '/$path')
@@ -39,7 +39,6 @@ class Builder {
       let err
       if (this.params.length !== pathParams.length) {
         err = new Error(`Invalid number of params in path (expected ${pathParams.length}, got ${this.params.length}).`)
-
         return fn(err)
       }
 
@@ -47,54 +46,18 @@ class Builder {
         requestPath = requestPath.replace(/{[^}]+}/, param)
       })
 
-      // check required payload properties
-      if (requiredProps && requiredProps.length > 0) {
-        if (!data) {
-          err = new Error('Missing parameters.')
-        } else {
-          for (let i = 0; i < requiredProps.length; i++) {
-            const prop = requiredProps[i]
-
-            if (typeof data[prop] === 'undefined') {
-              err = new Error(`Missing parameter '${prop}'`)
-              break
-            }
-          }
-        }
-      }
-
-      if (err) {
-        return fn(err)
-      }
-
-      // check payload property types
-      for (const key in properties) {
-        if (data && data[key]) {
-          const type = properties[key].type
-
-          let dataType = typeof data[key]
-
-          if (Array.isArray(data[key])) {
-            dataType = 'array'
-          }
-
-          if (Array.isArray(type)) {
-            if (type.indexOf(dataType) === -1) {
-              err = new Error(`Invalid parameter type. ${key} must be of type: ${type}.`)
-              break
-            }
-          } else if (dataType !== type) {
-            err = new Error(`Invalid parameter type. ${key} must be of type: ${type}.`)
-            break
-          }
-        }
-      }
-
+      let schema = { properties }
+      if (action.required) { schema.required = action.required }
+      err = ajv(schema, data)
       if (err) {
         return fn(err)
       }
 
       this.client = this.base
+      if (typeof this.client[action.method.toLowerCase()] !== 'function') {
+        console.error(`WARNING: 'this.client.${action.method.toLowerCase()}' is not a function`)
+        return
+      }
       return this.client[action.method.toLowerCase()](requestPath, data, fn)
     }
 
@@ -102,16 +65,8 @@ class Builder {
       return promisifyCall(this, impl, data, fn)
     }
 
-    constructor.prototype[getName(actionName)] = promisifed
+    constructor.prototype[_.camelCase(actionName)] = promisifed
   }
-}
-
-function getName (name) {
-  name = name.toLowerCase()
-  name = inflection.dasherize(name).replace(/-/g, '_')
-  name = inflection.camelize(name, true)
-
-  return name
 }
 
 function build (baseObj, resources) {
